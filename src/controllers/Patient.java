1ddb59110dbcc063bcc7426f4005d0138a2d88aa
AbigailPedro <abigailpedro2003@gmai.com>
2024-07-01 16:28:09 +0100

    add patient controller

---
+++ src/controllers/Patient.java
@@ -0,0 +1,66 @@
+package controllers;
+
+import java.sql.Connection;
+import java.time.LocalDate;
+
+import repositories.MySQLConnection;
+import repositories.PatientRepository;
+
+public class Patient {
+    String name;
+    LocalDate born;
+    String address;
+    String phone;
+
+    public Patient (String name, LocalDate date, String address, String phone) {
+        this.name = name;
+        this.born = date;
+        this.address = address;
+        this.phone = phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getBorn() {
+        return born;
+    }
+
+    public void setBorn(LocalDate born) {
+        this.born = born;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public boolean cadastrarPaciente () {
+        Connection conn = MySQLConnection.getConnection();
+        PatientRepository patient = new PatientRepository(conn);
+
+        try {
+            patient.create(this);
+
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
